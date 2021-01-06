08241afaa72a7ceaadd6659372828adbfda0dba1
RJ Hollberg <rj.hollberg@outlook.com>
2021-01-06 07:55:47 -0700

    Split basic utilities out into their own script file.

--- boxstarter/Initialize.ps1
+++ boxstarter/Initialize.ps1
@@ -234,69 +234,6 @@ Move-LibraryDirectory -libraryName "My Video"    -newPath "$Env:OneDrive\Videos"
 Move-LibraryDirectory -libraryName "Personal"    -newPath "$Env:OneDrive\Documents"
 
 #----------------------------------------------------------------------------------------------------
-# Install browsers
-#----------------------------------------------------------------------------------------------------
-
-# Google Chrome
-choco install -y "googlechrome"
-Remove-Item "$Env:PUBLIC\Desktop\Google Chrome.lnk"   -ErrorAction "Ignore"
-Remove-Item "$Env:OneDrive\Desktop\Google Chrome.lnk" -ErrorAction "Ignore"
-
-#----------------------------------------------------------------------------------------------------
-# Install utilities
-#----------------------------------------------------------------------------------------------------
-
-# 7-Zip
-choco install -y "7zip"
-
-# Piriform CCleaner
-choco install -y "ccleaner"
-Remove-Item "$Env:PUBLIC\Desktop\CCleaner.lnk" -ErrorAction "Ignore"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/TaffarelJr/config/main/apps/CCleaner.ini" -OutFile "$Env:ProgramFiles\CCleaner\ccleaner.ini" -UseBasicParsing
-
-# Piriform Defraggler
-choco install -y "defraggler"
-Remove-Item "$Env:PUBLIC\Desktop\Defraggler.lnk" -ErrorAction "Ignore"
-
-# Notepad++
-choco install -y "notepadplusplus"
-
-# SpaceSniffer
-choco install -y "spacesniffer"
-
-#----------------------------------------------------------------------------------------------------
-# Install additional cloud storage providers
-#----------------------------------------------------------------------------------------------------
-
-# Dropbox
-choco install -y "dropbox"
-
-# Google Backup and Sync
-choco install -y "google-backup-and-sync"
-Remove-Item "$Env:PUBLIC\Desktop\Google Docs.lnk"   -ErrorAction "Ignore"
-Remove-Item "$Env:PUBLIC\Desktop\Google Sheets.lnk" -ErrorAction "Ignore"
-Remove-Item "$Env:PUBLIC\Desktop\Google Slides.lnk" -ErrorAction "Ignore"
-
-#----------------------------------------------------------------------------------------------------
-# Install communications tools
-#----------------------------------------------------------------------------------------------------
-
-# Slack
-choco install -y "slack"
-
-# Zoom
-choco install -y "zoom" --install-arguments="ZNoDesktopShortCut=true ZRecommend=""DisableVideo=1;MuteVoipWhenJoin=1;AutoJoinVOIP=1"""
-Remove-Item "$Env:PUBLIC\Desktop\Zoom.lnk" -ErrorAction "Ignore"
-
-#----------------------------------------------------------------------------------------------------
-# Install basic graphics tools
-#----------------------------------------------------------------------------------------------------
-
-# Paint.net
-choco install -y "paint.net"
-Remove-Item "$Env:PUBLIC\Desktop\paint.net.lnk" -ErrorAction "Ignore"
-
-#----------------------------------------------------------------------------------------------------
 # Post
 #----------------------------------------------------------------------------------------------------
 
