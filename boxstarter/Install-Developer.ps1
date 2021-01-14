d27ae366688fbf0bd2171636b93552d5d8633725
RJ Hollberg <rj.hollberg@outlook.com>
2021-01-14 10:32:25 -0700

    Add headers to VSIX downloads.

--- boxstarter/Install-Developer.ps1
+++ boxstarter/Install-Developer.ps1
@@ -46,7 +46,18 @@ function Install-VsixPackage() {
         $packageUri = "$marketplace$anchor"
         $vsixFileName = "$Env:TEMP\$packageName.vsix"
         Write-Host "Attempting to download $packageUri ..."
-        Invoke-WebRequest -Uri $packageUri -OutFile $vsixFileName -UseBasicParsing
+        Invoke-WebRequest -Uri $packageUri -OutFile $vsixFileName -UseBasicParsing -Headers @{
+            "accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+            "accept-encoding" = "gzip, deflate, br"
+            "accept-language" = "en-US,en;q=0.9"
+            "dnt" = "1"
+            "sec-fetch-dest" = "document"
+            "sec-fetch-mode" = "navigate"
+            "sec-fetch-site" = "same-origin"
+            "sec-fetch-user" = "?1"
+            "upgrade-insecure-requests" = "1"
+            "user-agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+        }
         if (-Not (Test-Path $vsixFileName)) {
             Write-Error "Downloaded VSIX file could not be located"
             exit 1
