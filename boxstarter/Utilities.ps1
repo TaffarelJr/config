e28ed10c93eca769c532ce05e845904671b1d2d8
RJ Hollberg <rj.hollberg@outlook.com>
2021-01-20 11:03:10 -0700

    Move VSIX install function to 'Utilities' file.

--- boxstarter/Utilities.ps1
+++ boxstarter/Utilities.ps1
@@ -1,3 +1,10 @@
+# Set PowerShell preference variables
+$ErrorActionPreference = "Stop"
+
+# Set custom variables
+$vsInstallDir = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Professional\Common7\IDE"
+$vsMarketplace = "https://vsMarketplace.visualstudio.com"
+
 # Register HKEY_CLASSES_ROOT as an accessible drive
 New-PSDrive -Name "HKCR" -PSProvider "Registry" -Root "HKEY_CLASSES_ROOT" | out-null
 
@@ -76,6 +83,62 @@ function Set-WindowsSearchFileExtension {
         }; Pop-Location
 
         Write-Host "Done"
+    }
+}
+
+# Download & install specified Visual Studio extensions
+# Modified from https://gist.github.com/ScottHutchinson/b22339c3d3688da5c9b477281e258400
+function Install-VsixPackage() {
+    param (
+        [string]$packageName = $(throw "Please specify a Visual Studio Extension" )
+    )
+
+    # Scrape the VS Marketplace web page, to get the download path and VSIX ID
+    $packagePage = "$vsMarketplace/items?itemName=$packageName"
+    Write-Host "Scrape VSIX details from $packagePage"
+    $response = Invoke-WebRequest -Uri $packagePage -UseBasicParsing
+    $anchor = $response.Links | Where-Object { $_.class -eq "install-button-container" } | Select-Object -ExpandProperty "href"
+    if (-Not $anchor) {
+        Write-Error "Could not find download anchor tag on the Visual Studio Extensions page"
+        exit 1
+    }
+    $vsixId = $response.Content | Select-String -List -Pattern '"VsixId":"(.+?)"' | ForEach-Object { $_.Matches.Groups[1].Value }
+    if (-Not $vsixId) {
+        Write-Error "Could not find VSIX ID on the Visual Studio Extensions page"
+        exit 1
+    }
+
+    # Check to see if the extension is already installed
+    if ($installedVsExtensions -Contains $vsixId) {
+        Write-Host "Already installed. Skipping"
+    }
+    else {
+        # If not, then download it
+        $packageUri = "$vsMarketplace$anchor"
+        $vsixFileName = "$Env:TEMP\$packageName.vsix"
+        Write-Host "Attempting to download $packageUri ..."
+        Invoke-WebRequest -Uri $packageUri -OutFile $vsixFileName -UseBasicParsing -Headers @{
+            "accept"                    = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+            "accept-encoding"           = "gzip, deflate, br"
+            "accept-language"           = "en-US,en;q=0.9"
+            "dnt"                       = "1"
+            "sec-fetch-dest"            = "document"
+            "sec-fetch-mode"            = "navigate"
+            "sec-fetch-site"            = "same-origin"
+            "sec-fetch-user"            = "?1"
+            "upgrade-insecure-requests" = "1"
+            "user-agent"                = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+        }
+        if (-Not (Test-Path $vsixFileName)) {
+            Write-Error "Downloaded VSIX file could not be located"
+            exit 1
+        }
+
+        # Install the extension
+        Write-Host "Installing $vsixFileName ..."
+        Start-Process -Filepath "$vsInstallDir\VSIXInstaller.exe" -ArgumentList "/quiet /admin '$vsixFileName'" -Wait
+        Remove-Item $vsixFileName
+        Write-Host "Installation of $packageName complete!"
     }
 }
 
