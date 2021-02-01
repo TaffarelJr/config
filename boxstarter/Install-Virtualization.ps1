f012254fd55adae58ec5656c573d22476f3080be
RJ Hollberg <rj.hollberg@outlook.com>
2021-02-01 12:34:54 -0700

    Consolidate Windows edition checks in 'Virtualization' script.

--- boxstarter/Install-Virtualization.ps1
+++ boxstarter/Install-Virtualization.ps1
@@ -2,6 +2,9 @@
 Write-Host "Run startup scripts"
 #----------------------------------------------------------------------------------------------------
 
+# Validate Windows version
+if ($edition -EQ "Home") { throw [System.IO.InvalidOperationException] "This script cannot be run on Windows 10 Home Edition" }
+
 # Download & import utilities
 $repoUri = "https://raw.githubusercontent.com/TaffarelJr/config/main"
 $fileUri = "$repoUri/boxstarter/Utilities.ps1"
@@ -14,50 +17,41 @@ Invoke-WebRequest -Uri $fileUri -OutFile $filePath -UseBasicParsing
 Write-Header "Install Microsoft Hyper-V"
 #----------------------------------------------------------------------------------------------------
 
-if ($edition -EQ "Home") {
-    Write-Host "<not available on Home edition of Windows>"
-}
-else {
-    choco install -y $chocoCache "Microsoft-Hyper-V-All" -source "windowsfeatures"
-}
+# Let Boxstarter/Chocolatey install Hyper-V instead of Windows, so it can manage reboots
+choco install -y $chocoCache "Microsoft-Hyper-V-All" -source "windowsfeatures"
 
 #----------------------------------------------------------------------------------------------------
 Write-Header "Install Windows Subsystem for Linux 2 (WSL2)"
 #----------------------------------------------------------------------------------------------------
 
-if ($edition -EQ "Home") {
-    Write-Host "<not available on Home edition of Windows>"
-}
-else {
-    # Install WSL 2
-    choco install -y $chocoCache "wsl2" --package-parameters="/Retry:true"
+# Install WSL 2
+choco install -y $chocoCache "wsl2" --package-parameters="/Retry:true"
 
-    # Install Ubuntu
-    # https://docs.microsoft.com/en-us/windows/wsl/install-manual
-    choco install -y $chocoCache "wsl-ubuntu-2004"
+# Install Ubuntu
+# https://docs.microsoft.com/en-us/windows/wsl/install-manual
+choco install -y $chocoCache "wsl-ubuntu-2004"
 
-    # Launch Ubuntu and allow it to initialize
-    Write-Host "Initialize Ubuntu"
-    $ubuntu = Get-ChildItem "$Env:ProgramFiles\WindowsApps\" | `
-        ForEach-Object { Get-ItemProperty $_.PSPath } | `
-        Where-Object { $_ -Match "Ubuntu" } | `
-        Get-ChildItem | `
-        ForEach-Object { Get-ItemProperty $_.PSPath } | `
-        Where-Object { $_ -Match ".exe" }
-    start-process $ubuntu -Wait
+# Launch Ubuntu and allow it to initialize
+Write-Host "Initialize Ubuntu"
+$ubuntu = Get-ChildItem "$Env:ProgramFiles\WindowsApps\" | `
+    ForEach-Object { Get-ItemProperty $_.PSPath } | `
+    Where-Object { $_ -Match "Ubuntu" } | `
+    Get-ChildItem | `
+    ForEach-Object { Get-ItemProperty $_.PSPath } | `
+    Where-Object { $_ -Match ".exe" }
+start-process $ubuntu -Wait
 
-    # Configure Ubuntu
-    Write-Host "Run Ubuntu setup script"
-    wsl sudo sh -c "`$(curl -fsSL $repoUri/boxstarter/Ubuntu.sh)"
+# Configure Ubuntu
+Write-Host "Run Ubuntu setup script"
+wsl sudo sh -c "`$(curl -fsSL $repoUri/boxstarter/Ubuntu.sh)"
 
-    # Install Homebrew recipies
-    Write-Host "Run Homebrew setup script"
-    wsl sh -c "`$(curl -fsSL $repoUri/boxstarter/Homebrew.sh)"
+# Install Homebrew recipies
+Write-Host "Run Homebrew setup script"
+wsl sh -c "`$(curl -fsSL $repoUri/boxstarter/Homebrew.sh)"
 
-    # Reboot WSL to make sure changes take effect
-    Write-Host "Reboot WSL"
-    Restart-Service -Name "LxssManager"
-}
+# Reboot WSL to make sure changes take effect
+Write-Host "Reboot WSL"
+Restart-Service -Name "LxssManager"
 
 #----------------------------------------------------------------------------------------------------
 Write-Header "Install Docker"
