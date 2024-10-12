f04ef3c7f8bd7ade5a4c944a5d5cbd1b359c2f44
RJ Hollberg <rj.hollberg@outlook.com>
2024-10-12 14:51:28 -0600

    Add PowerShell function for confirming installations

---
+++ PowerShell/Modules/Confirmation.psm1
@@ -0,0 +1,37 @@
+﻿function Confirm-Installation {
+    <#
+        .SYNOPSIS
+            Tests an installation to make sure it succeeded.
+
+        .PARAMETER Label
+            An extra descriptor of the installation test, if needed.
+            Optional.
+
+        .PARAMETER ScriptBlock
+            The script block to run to test the installation.
+    #>
+
+    param(
+        [Parameter(Position = 0)]
+        [string] $Label = $null,
+
+        [Parameter(Position = 1, Mandatory)]
+        [ScriptBlock] $ScriptBlock
+    )
+
+    if ($Label) {
+        Write-Host "$Label confirmation: " -ForegroundColor 'DarkCyan' -NoNewline
+    }
+    else {
+        Write-Host "Confirmation: " -ForegroundColor 'DarkCyan' -NoNewline
+    }
+
+    & $ScriptBlock | Write-Host -ForegroundColor 'Green'
+    if (-not $?) {
+        throw "Failed to install $Label"
+    }
+}
+
+#-------------------------------------------------------------------------------
+
+Export-ModuleMember -Function Confirm-Installation
