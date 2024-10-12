58ab82b41366ed544cd4637fb2ccaf0a0b547bbf
RJ Hollberg <rj.hollberg@outlook.com>
2024-10-12 15:38:02 -0600

    Add PowerShell meta script for configuring home machines

---
+++ PowerShell/Machines/Home.ps1
@@ -0,0 +1,8 @@
+"$PSScriptRoot\..\Modules\*.psm1" | Get-ChildItem | Import-Module -Force
+Initialize-Environment
+Assert-ComputerName
+
+$componentDir = "$PSScriptRoot\..\Components"
+
+. "$componentDir\PackageManagers\_All.ps1" # Do this first
+. "$componentDir\OS\_All.ps1"
