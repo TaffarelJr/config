721f9b0852109907b59e72cfec86d730f93501db
RJ Hollberg <rj.hollberg@outlook.com>
2024-10-12 14:13:23 -0600

    Add PowerShell script for installing WinGet

---
+++ PowerShell/Components/PackageManagers/WinGet.ps1
@@ -0,0 +1,19 @@
+"$PSScriptRoot\..\..\Modules\*.psm1" | Get-ChildItem | Import-Module -Force
+Initialize-Environment
+
+#-------------------------------------------------------------------------------
+Start-Component 'WinGet'
+#-------------------------------------------------------------------------------
+
+# WinGet is included as part of the 'App Installer' app.
+# Since we don't know what other package managers are available,
+# use the bare PowerShell functions to install it.
+Get-AppxPackage -AllUsers 'Microsoft.DesktopAppInstaller' | ForEach-Object {
+    Write-host "Installing '$($_.Name)' ..."
+    Add-AppxPackage -Register "$($_.InstallLocation)\AppXManifest.xml" -ErrorAction 'SilentlyContinue'
+    Confirm-Installation -ScriptBlock { $_.Version }
+}
+
+# CLI and PowerShell module should also be available
+Confirm-Installation -Label 'CLI' -ScriptBlock { winget -v }
+Confirm-Installation -Label 'PowerShell module' -ScriptBlock { Get-WinGetVersion }
