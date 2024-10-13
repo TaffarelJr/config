fd68de9cb931562d33b04c65d4e918e8993d1c3b
RJ Hollberg <rj.hollberg@outlook.com>
2024-10-13 14:20:54 -0600

    Add profile scripting for customizing Windows Terminal

--- PowerShell/Profiles/RJ.ps1
+++ PowerShell/Profiles/RJ.ps1
@@ -9,9 +9,40 @@ Start-ComponentGroup 'Profile: RJ'
 
 $devDrive = 'C:\Dev'
 
+$devFont = [PSCustomObject]@{
+    Name    = 'FiraCode NF'
+    Package = 'firacodenf'
+}
+
 #-------------------------------------------------------------------------------
 Start-Component 'Windows Search'
 #-------------------------------------------------------------------------------
 
 # Indexing Options -> Index these locations:
 Assert-WindowsSearchLocation -Path $devDrive
+
+#-------------------------------------------------------------------------------
+Start-Component 'Fonts'
+#-------------------------------------------------------------------------------
+
+Assert-ChocolateyPackage -Name $devFont.Package
+
+#-------------------------------------------------------------------------------
+Start-Component 'Windows Terminal'
+#-------------------------------------------------------------------------------
+
+Write-Host 'Validating configuration ...'
+$config = Get-WindowsTerminalConfig
+
+$changed = `
+($config.profiles.defaults | Assert-Property -Name 'font') -bor `
+($config.profiles.defaults.font | Assert-PropertyValue -Name 'face' -Value $devFont.Name) -bor `
+($config.profiles.defaults.font | Assert-PropertyValue -Name 'size' -Value 11) -bor `
+($config.profiles.defaults.font | Assert-PropertyValue -Name 'weight' -Value 'normal') -bor `
+($config.profiles.defaults | Assert-PropertyValue -Name 'useAcrylic' -Value $True) -bor `
+($config.profiles.defaults | Assert-PropertyValue -Name 'opacity' -Value 80)
+
+if ($changed) {
+    Write-Host 'Saving changes...'
+    Set-WindowsTerminalConfig $config
+}
