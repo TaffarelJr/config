bf7ffd396edef222f4a97cc5ea64fd3951783dba
RJ Hollberg <rj.hollberg@outlook.com>
2024-10-13 14:39:57 -0600

    Add PowerShell script for installing PowerShell

---
+++ PowerShell/Components/Shells/PowerShell.ps1
@@ -0,0 +1,45 @@
+$moduleDir = "$PSScriptRoot\..\..\Modules"
+"$moduleDir\*.psm1" | Get-ChildItem | Import-Module -Force
+Initialize-Environment
+
+#-------------------------------------------------------------------------------
+Start-Component 'PowerShell 2'
+#-------------------------------------------------------------------------------
+
+# PowerShell 2 is now recognized as a security risk
+# that can be used to run malicious scripts.
+# It's replaced by PowerShell 5 in modern Windows.
+Write-Host "Removing ..."
+Disable-WindowsOptionalFeature -FeatureName 'MicrosoftWindowsPowerShellV2Root' -Online
+
+#-------------------------------------------------------------------------------
+Start-Component '(Windows) PowerShell 5'
+#-------------------------------------------------------------------------------
+
+# PowerShell 5 is built into (and updated with) Windows.
+# No need to install or update it here.
+
+Invoke-PowerShell5 -ModuleDir $moduleDir -ScriptBlock {
+    Assert-PowerShellConfiguration
+}
+
+#-------------------------------------------------------------------------------
+Start-Component 'PowerShell (Core) 7'
+#-------------------------------------------------------------------------------
+
+Assert-WinGetPackage -Name 'Microsoft.PowerShell'
+Update-SessionEnvironment
+
+Write-Host
+Invoke-PowerShell7 -ModuleDir $moduleDir -ScriptBlock {
+    Assert-PowerShellConfiguration
+}
+
+# Ensure PowerShell 7 is the default in Windows Terminal
+$config = Get-WindowsTerminalConfig
+$obj = $config.profiles.list | Where-Object { $_.Source -Like '*.PowershellCore' }
+if (($null -ne $obj) -and ($config.defaultProfile -ne $obj.guid)) {
+    Write-Host 'Making PowerShell 7 the default in Windows Terminal ...'
+    $config.defaultProfile = $obj.guid
+    Set-WindowsTerminalConfig $config
+}
