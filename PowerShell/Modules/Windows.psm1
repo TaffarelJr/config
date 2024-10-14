807886b8a2eee91a4f194cf6860b8e72520d20f9
RJ Hollberg <rj.hollberg@outlook.com>
2024-10-14 12:39:55 -0600

    Add PowerShell function for disabling Windows services

--- PowerShell/Modules/Windows.psm1
+++ PowerShell/Modules/Windows.psm1
@@ -22,4 +22,48 @@ function Assert-Admin {
 
 #-------------------------------------------------------------------------------
 
+function Disable-WindowsService {
+    <#
+        .SYNOPSIS
+            Ensures the specified Windows service is stopped
+            and set to manual start only.
+
+        .PARAMETER Name
+            The name of the Windows service.
+    #>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0, Mandatory, ValueFromPipeline)]
+        [string] $Name
+    )
+
+    process {
+        try {
+            # Get the specified service
+            $service = Get-Service -Name $Name
+        }
+        catch {
+            # If the service is not found, do nothing
+        }
+
+        if ($null -ne $service) {
+            # If the service is running, stop it
+            if ($service.Status -eq 'Running') {
+                Write-Host "Stopping $($service.DisplayName)"
+                Stop-Service -Name $Name
+            }
+
+            # If the service is set to start automatically, make it manual
+            if ($service.StartType -eq 'Automatic') {
+                Write-Host "Setting $($service.DisplayName) to manual start"
+                Set-Service -Name $Name -StartupType Manual
+            }
+        }
+    }
+}
+
+#-------------------------------------------------------------------------------
+
 Export-ModuleMember -Function Assert-Admin
+Export-ModuleMember -Function Disable-WindowsService
