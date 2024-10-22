070db9c8fdf29d835139a078b029ca73195eb957
RJ Hollberg <rj.hollberg@outlook.com>
2024-10-22 16:53:37 -0600

    Add PowerShell script for installing Firefox

---
+++ PowerShell/Components/Browsers/Firefox.ps1
@@ -0,0 +1,12 @@
+"$PSScriptRoot\..\..\Modules\*.psm1" | Get-ChildItem | Import-Module -Force
+Initialize-Environment
+
+#-------------------------------------------------------------------------------
+Start-Component 'Firefox (Developer Edition)'
+#-------------------------------------------------------------------------------
+
+# Core installation
+Assert-WinGetPackage -Name 'Mozilla.Firefox.DeveloperEdition'
+
+# Remove desktop shortcut
+Remove-FromWindowsDesktop -Name 'Firefox'
