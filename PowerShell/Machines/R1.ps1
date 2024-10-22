a166831fae8cf8f952fe8745f219f86237e1bda2
RJ Hollberg <rj.hollberg@outlook.com>
2024-10-22 16:44:17 -0600

    Add PowerShell script for installing system utilities

--- PowerShell/Machines/R1.ps1
+++ PowerShell/Machines/R1.ps1
@@ -7,7 +7,7 @@ $componentDir = "$PSScriptRoot\..\Components"
 . "$componentDir\PackageManagers\_All.ps1" # Do this first
 . "$componentDir\OS\_All.ps1"
 . "$componentDir\Shells\_All.ps1"
+. "$componentDir\Utilities\_All.ps1"
 
-# Developer Tools
+# Developer tools
 . "$componentDir\SourceControl\_All.ps1"
-
