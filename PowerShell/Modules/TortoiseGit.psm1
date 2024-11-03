cfa586814f32fb396fe6c363d945994abd17b4ec
RJ Hollberg <rj.hollberg@outlook.com>
2024-11-03 00:59:04 -0600

    Add PowerShell function for registering diff viewer in TortoiseGit

--- PowerShell/Modules/TortoiseGit.psm1
+++ PowerShell/Modules/TortoiseGit.psm1
@@ -38,4 +38,45 @@ function Set-TortoiseGitDiffTool {
 
 #-------------------------------------------------------------------------------
 
+function Set-TortoiseGitDiffViewer {
+    <#
+        .SYNOPSIS
+            Sets the diff viewer (patch file) command line used by TortoiseGit.
+
+        .PARAMETER Command
+            The command line to be used as the diff viewer.
+            Optional. Defaults to $null.
+            If this value is specified in combination with UseBuiltIn,
+            the command will still be stored--but will be disabled.
+
+        .PARAMETER UseBuiltIn
+            If specified, the built-in diff viewer is used.
+            If specified in combination with Command,
+            the command will still be stored--but will be disabled.
+    #>
+
+    param(
+        [Parameter(Position = 0)]
+        [string] $Command = $null,
+
+        [Parameter(Position = 1)]
+        [switch] $UseBuiltIn
+    )
+
+    # Mark the command as disabled, if necessary
+    if (($UseBuiltIn) -and ($null -ne $Command) -and ($Command.Length -gt 0)) {
+        $Command = "#$Command"
+    }
+
+    # Save the diff viewer command
+    Assert-RegistryValue `
+        -Hive CurrentUser `
+        -Key 'Software\TortoiseGit' `
+        -ValueName 'DiffViewer' `
+        -ValueData $Command
+}
+
+#-------------------------------------------------------------------------------
+
 Export-ModuleMember -Function Set-TortoiseGitDiffTool
+Export-ModuleMember -Function Set-TortoiseGitDiffViewer
