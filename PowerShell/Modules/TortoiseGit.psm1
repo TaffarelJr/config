18c8ccf55b0f8e7da85d7796293f173050b2d006
RJ Hollberg <rj.hollberg@outlook.com>
2024-11-03 01:11:09 -0600

    Add PowerShell function for registering merge tool in TortoiseGit

--- PowerShell/Modules/TortoiseGit.psm1
+++ PowerShell/Modules/TortoiseGit.psm1
@@ -78,5 +78,68 @@ function Set-TortoiseGitDiffViewer {
 
 #-------------------------------------------------------------------------------
 
+function Set-TortoiseGitMergeTool {
+    <#
+        .SYNOPSIS
+            Sets the merge tool command line used by TortoiseGit.
+
+        .PARAMETER Command
+            The command line to be used as the merge tool.
+            Optional. Defaults to $null.
+            If this value is specified in combination with UseBuiltIn,
+            the command will still be stored--but will be disabled.
+
+        .PARAMETER UseBuiltIn
+            If specified, the built-in merge tool is used.
+            If specified in combination with Command,
+            the command will still be stored--but will be disabled.
+
+        .PARAMETER Block
+            If specified, TortoiseGit will block while executing
+            the external merge tool.
+
+        .PARAMETER TrustExitCode
+            If specified, TortoiseGit will trust the exit code
+            of the external merge tool for auto-resolving.
+            Can only be used in combination with Block.
+    #>
+
+    param(
+        [Parameter(Position = 0)]
+        [string] $Command = $null,
+
+        [Parameter(Position = 1)]
+        [switch] $UseBuiltIn,
+
+        [Parameter(Position = 2)]
+        [switch] $Block,
+
+        [Parameter(Position = 3)]
+        [switch] $TrustExitCode
+    )
+
+    # Mark the command as disabled, if necessary
+    if (($UseBuiltIn) -and ($null -ne $Command) -and ($Command.Length -gt 0)) {
+        $Command = "#$Command"
+    }
+
+    # Save the merge tool command
+    Assert-RegistryValue `
+        -Hive CurrentUser `
+        -Key 'Software\TortoiseGit' `
+        -ValueName 'Merge' `
+        -ValueData $Command
+
+    # Save the block/trust configuration
+    Assert-RegistryValue `
+        -Hive CurrentUser `
+        -Key 'Software\TortoiseGit' `
+        -ValueName 'MergeBlockTrustBehavior' `
+        -ValueData (if ($Block) { if ($TrustExitCode) { 2 } else { 1 } } else { 0 })
+}
+
+#-------------------------------------------------------------------------------
+
 Export-ModuleMember -Function Set-TortoiseGitDiffTool
 Export-ModuleMember -Function Set-TortoiseGitDiffViewer
+Export-ModuleMember -Function Set-TortoiseGitMergeTool
